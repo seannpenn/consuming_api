42a4b911db6083fc9d076e8f8d21d06071002827
Seanpen <sean.pinote@usjr.edu.ph>
2022-07-10 22:57:09 +0800

    serialization on model added

--- lib/main.dart
+++ lib/main.dart
@@ -19,7 +19,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home:  NoteList(),
+      home:  const NoteList(),
     );
   }
 }
--- lib/models/note_for_listing.dart
+++ lib/models/note_for_listing.dart
@@ -2,29 +2,52 @@ class NoteForListing {
   String noteID;
   String noteTitle;
   String noteContent;
-  DateTime createDateTime;
-  DateTime latestEditDateTime;
+  late DateTime createDateTime;
+  late DateTime latestEditDateTime;
 
   NoteForListing(
       {this.noteID = '',
       this.noteTitle = '',
       this.noteContent = '',
       DateTime? createDateTime,
-      DateTime? latestEditDateTime}): createDateTime = createDateTime ?? DateTime.now(), latestEditDateTime = latestEditDateTime?? DateTime.now();
+      DateTime? latestEditDateTime}) {
+    createDateTime == null
+        ? this.createDateTime = DateTime.now()
+        : this.createDateTime = createDateTime;
+    latestEditDateTime == null
+        ? this.latestEditDateTime = DateTime.now()
+        : this.latestEditDateTime = latestEditDateTime;
+  }
 
   factory NoteForListing.fromJson(Map<String, dynamic> item) {
     return NoteForListing(
         noteID: item['noteID'],
         noteTitle: item['noteTitle'],
-        noteContent: item['noteContent']?? '',
-        createDateTime: DateTime.parse(item['createDateTime']),
-        latestEditDateTime: DateTime.parse(item['createDateTime']));
+        noteContent: item['noteContent'] ?? '',
+        createDateTime: item["createDateTime"] == null
+            ? null
+            : DateTime.parse(item['createDateTime']),
+        latestEditDateTime: item["latestEditDateTime"] == null
+            ? null
+            : DateTime.parse(item['latestEditDateTime']));
   }
 
-  Map<String, dynamic> toJson(){
-    return{
-      "noteTitle": noteTitle,
-      "noteContent": noteContent,
-    };
+  Map<String, dynamic> get json => {
+        'noteID': noteID,
+        'noteTitle': noteTitle,
+        'noteContent': noteContent,
+        'createDateTime': createDateTime.toString(),
+        'latestEditDateTime': latestEditDateTime.toString(),
+      };
+
+  Map<String, dynamic> toJson() {
+    // return {
+    //   "noteID": noteID,
+    //   "noteTitle": noteTitle,
+    //   "noteContent": noteContent,
+    //   // "createDateTime": createDateTime ,
+    //   // "latestEditDateTime": latestEditDateTime
+    // };
+    return json;
   }
 }
